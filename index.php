299daa80f74e0463ce20f5a7e864581ff5f653fc
danbrat <danielbratkowski@gmail.com>
2014-05-27 21:50:29 -0400

    added dan tintle's beautiful work

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+	$name = $_POST('name');
+	$email = $_POST('email');
+	$message = $_POST('message');
+	$from = 'From: UnoDosDan Contact';
+	$to = 'info@unodosdan.com'
+	$subject = "UnoDos, You're my only hope!"
+	$human = $_POST('human');
+
+	$body = "From: $name\n E-Mail: $email\n Message: $message";
+
+	if ($_POST['submit'] && $human == '4')	{
+		// anything in here is submitted on via form submission*/
+		if (mail ($to, $subject, $body, $from)) {
+			echo '<p>Your message has been sent!</p>';
+		}
+		else {
+			echo '<p>Something went wrong, go back and try again!</p>';
+		}
+		else if ($_POST['submit'] && $human != '4') {
+			echo '<p>You answered the anti-spam question incorrectly!</p>';
+		}
+	}
+
+
+
+
+<<<<<<< HEAD
+?>
+=======
+?>
+>>>>>>> 462de494e21162d173abf2f3c1205509de282127
